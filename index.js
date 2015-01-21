1e6b659856e5c73711d9b20ed078269f88e40ec2
markhuge <mark@segfawlt.net>
2015-01-21 02:53:35 -0800

    removed debug

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var thru = require('through2'),
 
 exports.config = function (opts) { mail.config(opts) }
 exports.send = function (obj) {
-  console.log('inside',obj)
   var stream = thru.obj(transform, flush); 
   if (obj) stream.write(obj);
   return stream;
